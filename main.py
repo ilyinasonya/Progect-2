0667056f70c254899b279eee5b4fc0e8f79aaf28
ilyinasonya <161287053+ilyinasonya@users.noreply.github.com>
2024-06-09 20:30:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests 
+#импортируем библиотеку requests, которая позволяет делать HTTP запросы к веб-страницам.
 from bs4 import BeautifulSoup
+#импортируем класс BeautifulSoup из библиотеки bs4, которая позволяет парсить HTML и XML документы.
 url = 'http://books.toscrape.com/catalogue/category/books/classics_6/index.html'
 parse_books_info(url)
